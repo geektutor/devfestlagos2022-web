acb70a767b266693c7d02cd80079708573ac4b40
adavize0 <helloadavize@outlook.com>
2022-09-15 02:16:25 +0100

    speakers page layout

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatExpansionModule } from '@angular/material/expansion';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [MatExpansionModule, MatMenuModule],
+  exports: [MatExpansionModule, MatMenuModule, MatDialogModule],
 })
 export class MaterialImportsModule {}
